90640492d4c3198ec9ca46ec8d102ebc41c8dd35
Luis <luis.lopez@cimat.mx>
2015-08-23 16:24:33 -0500

    QR update
    Funcion para obtener el qualitative reasoning del exitement

--- mindnalytics/QR.cs
+++ mindnalytics/QR.cs
@@ -8,6 +8,18 @@ namespace mindnalytics
 {
     class QR
     {
+        public int exitementScore(List<double> valores, int numSamples)
+        {
+            int exitementScore = 0;
 
+            for (int i = 0; i + numSamples < valores.Count(); i += numSamples)
+            {
+                double[] muestra = valores.GetRange(i, numSamples).ToArray();
+                double media = MathNet.Numerics.Statistics.ArrayStatistics.Mean(muestra);
+                exitementScore += media > 0.6 ? 1 : media < 0.4 ? -1 : 0;
+            }
+
+            return exitementScore;
+        }
     }
 }
