6a243f4afe064b76988da6fb8ef9e582e0892fde
Luis <luis.lopez@cimat.mx>
2015-09-01 15:41:26 -0500

    Nada importante

--- mindnalytics/Preview.Designer.cs
+++ mindnalytics/Preview.Designer.cs
@@ -28,13 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Preview));
             this.pictureParent = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureParent)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureParent
@@ -47,31 +42,11 @@
             this.pictureParent.TabIndex = 0;
             this.pictureParent.TabStop = false;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(193, 85);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(254, 256);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(523, 85);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(263, 244);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
-            // 
             // Preview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(940, 504);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureParent);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -80,8 +55,6 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Preview_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureParent)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -89,8 +62,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureParent;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
 
     }
 }
--- mindnalytics/Preview.cs
+++ mindnalytics/Preview.cs
@@ -13,13 +13,15 @@ namespace mindnalytics
     public partial class Preview : Form
     {
         List<PictureBox> listaImagenes = new List<PictureBox>();
+        System.Windows.Forms.PictureBox lastImage;
+        Bitmap lastPicture;
 
         public Preview(Dictionary<Asset, int> assetPreview)
         {
             
 
 
-            InitializeComponent();
+            /*InitializeComponent();
             var sortedDict = from entry in assetPreview orderby entry.Value ascending select entry;
             
             foreach (var item in sortedDict)
@@ -28,7 +30,57 @@ namespace mindnalytics
                 addImage(item.Key);
                 //pictureBox2.Image = Image.FromFile(item.Key.Path);
             }
+            */
+            InitializeComponent();
+            //this.TransparencyKey = Color.White;
+            
+            var sortedDict = from entry in assetPreview orderby entry.Value ascending select entry;
+            
+            /*ImageList myImages = new ImageList();
+            foreach (var item in sortedDict)
+            {
+                var pngImage = new Bitmap(item.Key.path);
+                myImages.Images.Add(pngImage);
+            }
 
+            myImages.TransparentColor = Color.White;
+            */
+            int i = 0;
+            foreach (var item in sortedDict)
+            {
+                //Console.WriteLine(item.Key.path);
+                //addImage(item.Key);
+                //pictureBox2.Image = Image.FromFile(item.Key.Path);
+                System.Windows.Forms.PictureBox pictureBoxX = new System.Windows.Forms.PictureBox();
+
+                if (i > 0)
+                {
+                    pictureBoxX.Parent = lastImage;
+                    pictureBoxX.BackgroundImage = lastPicture;
+                } else 
+                {
+                    this.BackgroundImage = new Bitmap(item.Key.path);
+                }
+
+                //pictureBoxX.Image = Image.FromFile(item.Key.path);
+                var pngImage = new Bitmap(item.Key.path);
+                //pictureBoxX.Image = SetAlpha(pngImage, 255);
+                //pngImage.MakeTransparent(Color.White);
+                pictureBoxX.Image = pngImage;
+                //pictureBoxX.BackColor = Color.Transparent;
+                pictureBoxX.Location = new Point(item.Key.x, item.Key.y);
+                pictureBoxX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+                
+
+                pictureBoxX.Name = "pictureBox2";
+                
+                this.Controls.Add(pictureBoxX);
+                listaImagenes.Add(pictureBoxX);
+
+                lastPicture = pngImage;
+                lastImage = pictureBoxX;
+                i++;
+            }
 
             
             /*
@@ -90,6 +142,34 @@ namespace mindnalytics
             }
 
             
-        }  
+        }
+
+        public Bitmap SetAlpha(Bitmap bmp, byte alpha)
+        {
+            if (bmp == null) throw new ArgumentNullException("bmp");
+
+            var data = bmp.LockBits(
+                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            var line = data.Scan0;
+            var eof = line + data.Height * data.Stride;
+            while (line != eof)
+            {
+                var pixelAlpha = line + 3;
+                var eol = pixelAlpha + data.Width * 4;
+                while (pixelAlpha != eol)
+                {
+                    System.Runtime.InteropServices.Marshal.WriteByte(
+                        pixelAlpha, alpha);
+                    pixelAlpha += 4;
+                }
+                line += data.Stride;
+            }
+            bmp.UnlockBits(data);
+            return bmp;
+        }
+
     }
 }
